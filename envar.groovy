f732b8d52bbb65f471cd5ee3e3c51096e61dd269
Sonali-K <52071198+Sonali-K@users.noreply.github.com>
2020-10-12 12:03:45 +0530

    Update envar.groovy

--- envar.groovy
+++ envar.groovy
@@ -1,1 +1,7 @@
-
+// Security Testing ZAP Config variables
+         host = 'localhost'
+         port = '8090'
+         timeout= '500'
+         zapHome = '/home/ecgc-cicd/Downloads/ZAP_2.7.0/'
+         host1 = 'http://10.212.0.72:8082/'
+         allowedHosts = 'github.com'
